ec773af5c3d58c4d0a5223e341dc26f2a26138f9
gabriele salvato <gabriele.salvato@cnr.it>
2017-12-11 09:19:08 +0100

    inserted missing Line Feed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -341,6 +341,7 @@ MainWindow::on_startRvsTButton_clicked() {
                        .arg("I-Photo[A]\n", 12)
                        .toLocal8Bit());
     pOutputFile->write(configureRvsTDialog.sSampleInfo.toLocal8Bit());
+    pOutputFile->write("\n");
     pOutputFile->write(QString("Grating #= %1 Wavelength = %2 nm")
                                .arg(configureRvsTDialog.iGratingNumber)
                                .arg(configureRvsTDialog.dWavelength).toLocal8Bit());
